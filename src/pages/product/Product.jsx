5d0038ee1429a3bf24d75f0011b8a3d357eeb160
Vishnu S <vishnushanmugam1111@gmail.com>
2025-08-28 16:41:14 +0530

    addproductscss

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -521,6 +521,9 @@ const Product = () => {
             className={styles.addButton}
             onClick={() => setShowAddModal(true)}
           >
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" viewBox="0 0 16 16" style={{marginRight: '5px'}}>
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+            </svg>
             Add Product
           </button>
         </div>
